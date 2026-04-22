fa4979c43e407794edf272f5f4dcc289f0f0b838
yash1008-gh <yash.ssingh20046@gmail.com>
2026-04-22 20:22:10 +0530

    deploment configs

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
 
-app = Flask(__name__)
+application = Flask(__name__)
+app = application
+
 
 @app.route('/')
 def index():
@@ -33,4 +35,4 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",debug=True)
+    app.run(host="0.0.0.0")
